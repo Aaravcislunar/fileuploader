9fd6339ce56fe1f728107b45ff729d407191fce7
Aarav <aaravgupta1.ag@gmail.com>
2023-07-11 13:23:46 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      await axios.post('http://localhost:3001/upload', formData);
+
+      console.log('File uploaded successfully!');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className='conatiner'>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleSubmit} className='upload'>Upload</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const multer = require('multer');
+const Client = require('ssh2-sftp-client');
+const cors = require('cors');
+
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+app.use(cors());
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    const { file } = req;
+    const sftp = new Client();
+
+    await sftp.connect({
+      host: '192.168.101.10',
+      port: '22',
+      username: 'USERNAME',
+      password: 'aarav@123'
+    });
+    const remotePath = '/upload';
+
+    await sftp.put(file.path, `${remotePath}/${file.originalname}`);
+    await sftp.end();
+
+    res.send('File uploaded successfully!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error uploading file.');
+  }
+});
+
+app.listen(3001, () => {
+  console.log('Server listening on port 3001');
+});
